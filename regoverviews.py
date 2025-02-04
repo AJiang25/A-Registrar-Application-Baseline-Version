b837a69b4ff19afae04c03d11ee8a280bfde3c3e
Arnold Jiang <aj9183@princeton.edu>
2025-02-04 17:29:41 -0500

    Added comments about corner cases

--- regoverviews.py
+++ regoverviews.py
@@ -1,5 +1,6 @@
 #-----------------------------------------------------------------------
 # imports
+import sys
 import sqlite3
 import textwrap
 import argparse
@@ -35,6 +36,9 @@ def displayClasses(dept = None, num = None, area = None, title = None):
             descriptors.append(f"%{descriptor}%")
 
         query += "WHERE " + " AND ".join(conditions)
+        # The rows must sorted. The primary sort must be by dept in ascending order, 
+        # the secondary sort must be by coursenum in ascending order, and tertiary sort 
+        # must be by classid in ascending order.
         query += "ORDER BY cr.dept, cr.coursenum;"
         curser.execute(query, descriptors)
         ans = curser.fetchall()
@@ -42,12 +46,23 @@ def displayClasses(dept = None, num = None, area = None, title = None):
         print('%5s %4s %6s %4s %s' % ("ClsId", "Dept", "CrsNum", "Area", "Title"))
         print('%5s %4s %6s %4s %s' % ("-----", "----", "------", "----", "-----"))
 
+        # Within each row, each line must consist of no more than 72 characters, 
+        # not including the newline character.
+        # line = line.rstrip(’\n’)
+        
+        # Within each row, each line must end after a word, not within a word. 
+        # That is, no newline characters may appear within words.
         for row in ans:
             res = '%5s %4s %6s %4s %s' % (row[0], row[1], row[2], row[3], row[4])
             print(textwrap.fill(res, subsequent_indent=" "*23))
 
+        #  Normally your regoverviews.py must terminate with exit status 0. 
+        # If it detects a database-related error, then it must terminate with exit status 1. 
+        # If it detects erroneous command-line arguments, 
+        # then it must terminate with exit status 2 — as is the default behavior of argparse.
 
-        sqliteConnection.close()       
+        sqliteConnection.close()  
+             
 
 #-----------------------------------------------------------------------
 def main():
