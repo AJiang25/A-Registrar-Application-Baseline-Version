d656965a400d3d02b542659576b6512e5a075095
Amanda Chan <ac2921@princeton.edu>
2025-02-05 17:35:03 -0500

    implemented sql for regdetails.py

--- regdetails.py
+++ regdetails.py
@@ -9,16 +9,51 @@ import argparse
 
 def displayClassInfo(curser, classid = None): 
     query = """
-            
+            SELECT DISTINCT cl.classid, cl.courseid, cl.days, cl.starttime, 
+            cl.endtime, cl.endtime, cl.bldg, cl.roomnum, c.area, c.title, 
+            c.descrip, c.prereqs, cr.dept, cr.coursenum, p.profname
+            FROM classes cl
+            JOIN crosslistings cr ON c.courseid = cr.courseid 
+            JOIN courses c ON c.courseid = cl.courseid
+            JOIN coursesprofs cp ON cr.courseid = cp.courseid
+            JOIN profs p ON cp.profid = p.profid
+            WHERE cl.classid = ?
         """
-    curser.execute(query, classid)
+    descriptor = f"{classid}"
+
+    #query += "ORDER BY cr.dept ASC, cr.coursenum ASC, cl.classid ASC;"
+    curser.execute(query, [descriptor])
     ans = curser.fetchall()
 
+    print('-------------')
+    print('Class Details')
+    print('-------------')
 
+
+    labels = ["Class Id: ", "Days: ", "Start time: ", "End time: ", "Building: ", "Room: "]
+    for row in ans:
+        res = (f"Class Id: {row[0]}")
+        res+= (f"Days: %s" % row[1])
+        res+= (f"Start time: %s" % row[2])
+        res+= (f"End time: %s" % row[3])
+        res+= (f"Building: %s" % row[4])
+        res+= (f"Room: %s" % row[5])
+
+        #res = f"Course information:{'\n'.join(ans[0])}"
+        #res = '%5s %4s %6s %4s %s %s %s %s %s %s %s %s %s %s %s' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14])
+        print(textwrap.fill(res, width = 72, break_long_words= False, subsequent_indent=" "*23))
+
+    
+
+    print('-------------')
+    print('Course Details')
+    print('-------------')
+        
 #-----------------------------------------------------------------------  
 def main():
     parser = argparse.ArgumentParser(description = 'Registrar application: show details about a class')
     parser.add_argument('classid', help='the id of the class whose details should be shown')
+    
 
     try:
         # Connects to the database and creates a curser connection 
@@ -29,7 +64,7 @@ def main():
         args = parser.parse_args()
         
         # Calls the displayClassInfo function 
-        displayClassInfo(curser = curser, classid = argsv[0])
+        displayClassInfo(curser = curser, classid=sys.argv[1])
         sys.exit(0)
 
     except sqlite3.Error:
@@ -43,6 +78,7 @@ def main():
         if sqliteConnection:
             sqliteConnection.close() 
 
+#-----------------------------------------------------------------------
 
-
-#----------------
+if __name__ == '__main__':
+    main()
