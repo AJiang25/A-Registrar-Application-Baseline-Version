c9f973d4a06792f7550586c8684db6d76eaa7483
Arnold Jiang <aj9183@princeton.edu>
2025-02-04 19:12:01 -0500

    Added some exceptions and resolved some corner cases

--- regoverviews.py
+++ regoverviews.py
@@ -6,9 +6,7 @@ import textwrap
 import argparse
 
 #-----------------------------------------------------------------------
-def displayClasses(dept = None, num = None, area = None, title = None):
-        sqliteConnection = sqlite3.connect('reg.sqlite')
-        curser = sqliteConnection.cursor()
+def displayClasses(curser, dept = None, num = None, area = None, title = None):   
         query = """
             SELECT DISTINCT cl.classid, cr.dept, cr.coursenum, c.area, c.title 
             FROM courses c 
@@ -34,12 +32,11 @@ def displayClasses(dept = None, num = None, area = None, title = None):
             conditions.append("c.title LIKE ? ESCAPE '\\'")
             descriptor = title.lower().replace("%", r"\%").replace("_", r"\_")
             descriptors.append(f"%{descriptor}%")
-
-        query += "WHERE " + " AND ".join(conditions)
-        # The rows must sorted. The primary sort must be by dept in ascending order, 
-        # the secondary sort must be by coursenum in ascending order, and tertiary sort 
-        # must be by classid in ascending order.
-        query += "ORDER BY cr.dept, cr.coursenum;"
+        if conditions:
+            query += "WHERE " + " AND ".join(conditions)
+            
+        query += "ORDER BY cr.dept ASC, cr.coursenum ASC, cl.classid ASC;"
+        print(query)
         curser.execute(query, descriptors)
         ans = curser.fetchall()
 
@@ -48,22 +45,12 @@ def displayClasses(dept = None, num = None, area = None, title = None):
 
         # Within each row, each line must consist of no more than 72 characters, 
         # not including the newline character.
-        # line = line.rstrip(’\n’)
-        
         # Within each row, each line must end after a word, not within a word. 
         # That is, no newline characters may appear within words.
         for row in ans:
             res = '%5s %4s %6s %4s %s' % (row[0], row[1], row[2], row[3], row[4])
-            print(textwrap.fill(res, subsequent_indent=" "*23))
-
-        #  Normally your regoverviews.py must terminate with exit status 0. 
-        # If it detects a database-related error, then it must terminate with exit status 1. 
-        # If it detects erroneous command-line arguments, 
-        # then it must terminate with exit status 2 — as is the default behavior of argparse.
-
-        sqliteConnection.close()  
-             
-
+            print(textwrap.fill(res, width = 72, break_long_words= False, subsequent_indent=" "*23))
+        
 #-----------------------------------------------------------------------
 def main():
     parser = argparse.ArgumentParser(description = 'Registrar application: show overviews of classes')
@@ -71,9 +58,39 @@ def main():
     parser.add_argument('-n', type=str, metavar = 'num', help ='show only those classes whose course number contains num')
     parser.add_argument('-a', type=str, metavar = 'area', help ='show only those classes whose distrib area contains area')
     parser.add_argument('-t', type=str, metavar = 'title', help ='show only those classes whose course title contains title')
-    args = parser.parse_args()
-
-    displayClasses(dept = args.d, num = args.n, area = args.a, title = args.t)
+        
+    # # Parses the stdin arguments
+    # args = parser.parse_args()
+        
+    # # Calls the displayClasses function 
+    # displayClasses(dept = args.d, num = args.n, area = args.a, title = args.t)
+    
+    try:
+        # Connects to the database and creates a curser connection 
+        sqliteConnection = sqlite3.connect('reg.sqlite')
+        curser = sqliteConnection.cursor()
+        
+        # Parses the stdin arguments
+        args = parser.parse_args()
+        
+        # Calls the displayClasses function 
+        displayClasses(curser = curser, dept = args.d, num = args.n, area = args.a, title = args.t)
+        sys.exit(0)
+        
+    # Normally your regoverviews.py must terminate with exit status 0. 
+    # If it detects a database-related error, then it must terminate with exit status 1. 
+    # If it detects erroneous command-line arguments, 
+    # then it must terminate with exit status 2 — as is the default behavior of argparse.
+    except sqlite3.Error:
+        #ASK ABOUT THE PRINT STATEMENTS
+        print(f"{sys.argv[0]}: {str(e)}", file=sys.stderr)
+        sys.exit(1)
+    except Exception as e:
+        print(f"{sys.argv[0]}: {str(e)}", file=sys.stderr) 
+        sys.exit(2)
+    finally: 
+        if sqliteConnection:
+            sqliteConnection.close() 
     
 #-----------------------------------------------------------------------
 
